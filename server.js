3b66d28ecf705fcc22cc47532c9e8061c12547a4
xavihachem <hachem02000@gmail.com>
2025-08-15 23:35:23 +0100

    origintest

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ const corsOptions = {
   origin: function (origin, callback) {
     // Allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
-    
+    console.log("Allowed origins",allowedOrigins);
     if (allowedOrigins.indexOf(origin) === -1) {
       const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
       console.error(msg);
